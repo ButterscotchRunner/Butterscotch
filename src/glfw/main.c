26cc7c675d0b5cd2c63df16b084ffa5e7396d81c
Un1q32 <joey.t.reinhart@gmail.com>
2026-05-09 06:52:25 -0400

    fix

--- src/glfw/main.c
+++ src/glfw/main.c
@@ -21,9 +21,7 @@
 #include "runner.h"
 #include "input_recording.h"
 #include "debug_overlay.h"
-#ifdef ENABLE_MODERN_GL
 #include "gl_renderer.h"
-#endif
 #ifdef ENABLE_LEGACY_GL
 #include "gl_legacy_renderer.h"
 #endif
