cee3eca5f98c7e3ea0ce669cd38015d351daf93f
Un1q32 <joey.t.reinhart@gmail.com>
2026-05-17 20:09:23 -0400

    common dot h

--- src/binary_utils.h
+++ src/binary_utils.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "common.h"
-#include "endianness.h"
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
--- src/endianness.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#if (defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)) || defined(__BIG_ENDIAN__)
-#define IS_BIG_ENDIAN
-#endif
