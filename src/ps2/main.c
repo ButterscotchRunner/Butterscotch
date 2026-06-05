20d45484ad7c888940bcd573209b2191a1072d62
rodrigo1593-dev <rcaneda22@gmail.com>
2026-06-05 16:59:51 -0300

    Update main.c

--- src/ps2/main.c
+++ src/ps2/main.c
@@ -290,7 +290,7 @@ int main(int argc, char* argv[]) {
     PS2Overlay_drawStatusScreen(nullptr, "Initializing controller...", false);
 
     int ret;
-    ret = SifExecModuleBuffer(freesio2_irx, size_freesio2_irx, 0, nullptr, nullptr);
+    ret = 1;//SifExecModuleBuffer(freesio2_irx, size_freesio2_irx, 0, nullptr, nullptr);
     if (0 > ret) {
         printf("Failed to load freesio2: %d\n", ret);
         return 1;
@@ -310,7 +310,7 @@ int main(int argc, char* argv[]) {
         printf("Failed to init libmc: %d\n", ret);
         return 1;
     }
-    ret = SifExecModuleBuffer(freepad_irx, size_freepad_irx, 0, nullptr, nullptr);
+    ret = 1;//SifExecModuleBuffer(freepad_irx, size_freepad_irx, 0, nullptr, nullptr);
     if (0 > ret) {
         printf("Failed to load freepad: %d\n", ret);
         return 1;
