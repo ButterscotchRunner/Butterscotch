0a8f9b67920d7e4de21b46b5f2f517ebb42efb1b
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-06 18:32:39 -0400

    add stuff

--- src/gettime.c
+++ src/gettime.c
@@ -15,6 +15,8 @@
 #endif
 #endif
 
+uint64_t game_start_time = 0;
+
 uint64_t nowNanos(void) {
 #if defined(PLATFORM_PS2)
     // kBUSCLK is bus clock ticks per second (~147 MHz).
--- src/gettime.h
+++ src/gettime.h
@@ -2,4 +2,6 @@
 
 #include <stdint.h>
 
+extern uint64_t game_start_time;
+
 uint64_t nowNanos(void);
