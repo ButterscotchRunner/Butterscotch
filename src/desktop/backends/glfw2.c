7d515cd5ca44922181ae08e5a89fd8b78b7190e8
cobaltgit <cobaltsecuremail312@proton.me>
2026-06-26 15:20:23 +0100

    also remove core from glfw2 for the <5 people who use that windowing frontend

--- src/desktop/backends/glfw2.c
+++ src/desktop/backends/glfw2.c
@@ -31,7 +31,6 @@ static bool tryOpenWindow(int reqW, int reqH) {
         glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, GLCommon_versions[i].minor);
             
         if (GLCommon_versions[i].major >= 3) {
-            glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
             if (GLCommon_versions[i].major == 3 && GLCommon_versions[i].minor == 2) {
                 glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
             } else {
