575d7c57aaf45e3f361a0f4f109903cf714132cf
cobaltgit <cobaltsecuremail312@proton.me>
2026-06-26 08:35:48 +0100

    what

--- src/gl/gl_renderer.c
+++ src/gl/gl_renderer.c
@@ -316,7 +316,7 @@ static void glInit(Renderer* renderer, DataWin* dataWin) {
     gl->hasVAO = false;
     bool hasFBO = false;
 
-    if (glGetStringi) {
+    if (glad_glGetStringi != nullptr) {
         GLint numExtensions;
         glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
         for (GLint i = 0; i < numExtensions; i++) {
