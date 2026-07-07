82b29d11df2b1ce2ca3b72800c744f1213fd8910
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-06 21:18:54 -0400

    rename function

--- src/desktop/main.c
+++ src/desktop/main.c
@@ -74,7 +74,7 @@ const GLuint *hostFramebuffer;
 #include <unistd.h>
 #endif
 
-static size_t get_anon_rss_bytes(void) {
+static size_t get_used_memory(void) {
 #ifdef __linux__
     int fd = open("/proc/self/smaps_rollup", O_RDONLY);
     if (fd < 0)
@@ -1791,7 +1791,7 @@ int main(int argc, char* argv[]) {
             }
 
             if (RunnerKeyboard_checkPressed(runner->keyboard, VK_BACKSPACE)) {
-                size_t bytes_used = get_anon_rss_bytes();
+                size_t bytes_used = get_used_memory();
                 if (bytes_used == 0)
                     fprintf(stderr, "Unable to get memory usage\n");
                 else
