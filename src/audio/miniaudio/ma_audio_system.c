db2eec1f5255e6dbd8e06043fb0e863d91a729c0
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-18 11:59:11 +0100

    make MA system

--- src/audio/miniaudio/ma_audio_system.c
+++ src/audio/miniaudio/ma_audio_system.c
@@ -12,11 +12,6 @@
 #if defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-#pragma GCC diagnostic ignored "-Wtype-limits"
-#pragma GCC diagnostic ignored "-Wsign-compare"
-#pragma GCC diagnostic ignored "-Wunused-result"
 #endif
 #include "miniaudio.h"
 #if defined(__GNUC__) || defined(__clang__)
