9b08a78c006854684757318abd5f52f7b9ae2607
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-19 15:53:04 +0100

    whoops

--- src/gl_common/gl_wrappers.h
+++ src/gl_common/gl_wrappers.h
@@ -38,23 +38,4 @@ static inline void gl_init_wrappers(void) {
         glBlendFuncSeparate = glBlendFuncSeparateEXT;
 }
 
-static inline void glBindFramebufferCached(GLRenderer* gl, GLenum target, GLuint fbo) {
-    if (target == GL_FRAMEBUFFER || target == GL_DRAW_FRAMEBUFFER)
-        gl->state.currentFbo = fbo;
-    glBindFramebuffer(target, fbo);
-}
-
-static inline void glViewportCached(GLRenderer* gl, int32_t x, int32_t y, int32_t w, int32_t h) {
-    gl->state.viewport[0] = x; gl->state.viewport[1] = y;
-    gl->state.viewport[2] = w; gl->state.viewport[3] = h;
-    glViewport(x, y, w, h);
-}
-
-static inline void glSetCap(GLRenderer* gl, GLenum cap, bool enable) {
-    if (cap == GL_SCISSOR_TEST) gl->state.scissorEnabled = enable;
-    else if (cap == GL_BLEND) gl->state.blendEnabled = enable;
-    if (enable) glEnable(cap);
-    else glDisable(cap);
-}
-
 #endif
