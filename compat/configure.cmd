ef7b02af99fd71ff7cc0878df2140d09daa80b4a
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-22 08:38:23 -0400

    stdbool check

--- compat/configure.cmd
+++ compat/configure.cmd
@@ -11,6 +11,19 @@ cd /d "%~dp0"
 if not exist tmp mkdir tmp
 
 type nul > tmp\config.log
+> config.bat echo @echo off
+
+>tmp\test.c echo.#include ^<stdbool.h^>
+>>tmp\test.c echo.int main(void^){return 0;}
+
+echo.checking for stdbool.h: >> tmp\config.log
+%CC% /nologo /Oi- tmp\test.c /c /Fo:tmp\test.obj >> tmp\config.log 2>&1
+if %errorlevel% neq 0 (
+    echo checking for stdbool.h: no
+    >>config.bat echo set INCLUDES=%%INCLUDES%% /Icompat\stdbool
+) else (
+    echo checking for stdbool.h: yes
+)
 
 >tmp\test.c echo.#include ^<stdio.h^>
 >>tmp\test.c echo.int main(void^){
@@ -22,10 +35,9 @@ echo.checking for snprintf: >> tmp\config.log
 %CC% /nologo /Oi- tmp\test.c /c /Fo:tmp\test.obj >> tmp\config.log 2>&1
 if %errorlevel% neq 0 (
     echo checking for snprintf: no
-    > config.bat echo @echo off
-    >> config.bat echo set DEFINES=%%DEFINES%% /DNO_SNPRINTF
-    >> config.bat echo set INCLUDES=%%INCLUDES%% /Icompat\stdio
-    >> config.bat echo set SRCS=%%SRCS%% compat\stdio\nanoprintf_impl.c
+    >>config.bat echo set DEFINES=%%DEFINES%% /DNO_SNPRINTF
+    >>config.bat echo set INCLUDES=%%INCLUDES%% /Icompat\stdio
+    >>config.bat echo set SRCS=%%SRCS%% compat\stdio\nanoprintf_impl.c
 ) else (
     echo checking for snprintf: yes
 )
