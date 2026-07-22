5d128753960284f38e86c616a6c78e839297391b
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-22 13:19:15 -0400

    fix

--- src/stdio_compat.h
+++ src/stdio_compat.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 
 #ifdef NO_SNPRINTF
-#define PRINTF_ALIAS_STANDARD_FUNCTION_NAMES_HARD 1
 #include <printf.h>
+#define snprintf snprintf_
+#define vsnprintf vsnprintf_
 #endif
 
 #endif /* _BS_COMPAT_STDIO_H_ */
