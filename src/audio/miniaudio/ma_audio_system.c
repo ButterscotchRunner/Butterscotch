366c98408fb2e153f7c5f75653e65d813168e992
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-24 22:00:37 +0100

    fix(openal): add back lazy audio, remove mmap for now (will be in a separate PR to investigate memory savings)

--- src/audio/miniaudio/ma_audio_system.c
+++ src/audio/miniaudio/ma_audio_system.c
@@ -755,8 +755,6 @@ static void maGroupLoad(AudioSystem* audio, int32_t groupIndex) {
         DataWinParserOptions options = {0};
         options.parseAudo = true;
         options.lazyLoadAudio = audio->dw->lazyLoadAudio;
-        if (audio->dw->mappedFile)
-            options.loadType = DATAWINLOADTYPE_MAP_FILE;
         DataWin *audioGroup = DataWin_parse(((MaAudioSystem*)audio)->fileSystem->vtable->resolvePath(((MaAudioSystem*)audio)->fileSystem, buf), options);
         arrput(audio->audioGroups, audioGroup);
         free(buf);
