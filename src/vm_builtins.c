c943534f6a52416ee570622ea19745e847c0a461
emiyl <me@emiyl.com>
2026-07-24 15:36:36 +0100

    use safestrdup

--- src/vm_builtins.c
+++ src/vm_builtins.c
@@ -15092,7 +15092,7 @@ static RValue builtin_path_add(VMContext* ctx, MAYBE_UNUSED RValue* args, MAYBE_
 
     char buffer[32];
     snprintf(buffer, sizeof(buffer), "__newpath%u", newIdx);
-    p->name = strdup(buffer);
+    p->name = safeStrdup(buffer);
 
     p->isSmooth = false;
     p->isClosed = true;
