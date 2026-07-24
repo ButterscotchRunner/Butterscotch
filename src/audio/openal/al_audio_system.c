163d44a2bafb7fb04ec5c319876ee603109d3fd2
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-24 22:02:27 +0100

    perf(audio): mmap audiogroup*.dat if main data.win is being mapped

--- src/audio/openal/al_audio_system.c
+++ src/audio/openal/al_audio_system.c
@@ -945,6 +945,8 @@ static void maGroupLoad(AudioSystem* audio, int32_t groupIndex) {
 
         DataWinParserOptions options = {0};
         options.parseAudo = true;
+        if (audio->dw->mappedFile)
+            options.loadType = DATAWINLOADTYPE_MAP_FILE;
         DataWin *audioGroup = DataWin_parse(((AlAudioSystem*)audio)->fileSystem->vtable->resolvePath(((AlAudioSystem*)audio)->fileSystem, buf), options);
         arrput(audio->audioGroups, audioGroup);
         free(buf);
