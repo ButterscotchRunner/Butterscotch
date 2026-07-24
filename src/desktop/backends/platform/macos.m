a845acfaa823f71721107e911a342c1e65e611d5
emiyl <me@emiyl.com>
2026-07-25 00:48:31 +0100

    make the error message work on 10.3 and up

--- src/desktop/backends/platform/macos.m
+++ src/desktop/backends/platform/macos.m
@@ -2,16 +2,33 @@
 
 void show_error_box(const char *message)
 {
+#if __has_feature(objc_arc)
     @autoreleasepool {
+#else
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+#endif
+
         NSAlert *alert = [[NSAlert alloc] init];
 
         [alert setMessageText:@"Error"];
-        [alert setInformativeText:
-            [NSString stringWithUTF8String:message]];
-
+        [alert setInformativeText:[NSString stringWithUTF8String:message]];
         [alert addButtonWithTitle:@"OK"];
+
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 101200
         [alert setAlertStyle:NSAlertStyleCritical];
+#else
+        [alert setAlertStyle:NSCriticalAlertStyle];
+#endif
 
         [alert runModal];
+
+#if !__has_feature(objc_arc)
+        [alert release];
+#endif
+
+#if __has_feature(objc_arc)
     }
+#else
+    [pool drain];
+#endif
 }
