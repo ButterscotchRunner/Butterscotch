19b6569db63501c7fae8a30fefa83edb8c588689
emiyl <me@emiyl.com>
2026-07-25 01:03:38 +0100

    separate into two functions

--- src/desktop/backends/platform/macos.m
+++ src/desktop/backends/platform/macos.m
@@ -1,10 +1,9 @@
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1030
-#import <Cocoa/Cocoa.h>
-#else
+#include <AvailabilityMacros.h>
 #import <AppKit/AppKit.h>
-#endif
 
-void show_error_box(const char *message)
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1030
+
+static void show_error_box_nsalert(const char *message)
 {
 #if __has_feature(objc_arc)
     @autoreleasepool {
@@ -12,36 +11,21 @@ void show_error_box(const char *message)
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 #endif
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1030
+    NSAlert *alert = [[NSAlert alloc] init];
 
-        NSAlert *alert = [[NSAlert alloc] init];
-
-        [alert setMessageText:@"Error"];
-        [alert setInformativeText:[NSString stringWithUTF8String:message]];
-        [alert addButtonWithTitle:@"OK"];
-
+    [alert setMessageText:@"Error"];
+    [alert setInformativeText:[NSString stringWithUTF8String:message]];
+    [alert addButtonWithTitle:@"OK"];
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= 101200
         [alert setAlertStyle:NSAlertStyleCritical];
 #else
         [alert setAlertStyle:NSCriticalAlertStyle];
 #endif
 
-        [alert runModal];
+    [alert runModal];
 
 #if !__has_feature(objc_arc)
-        [alert release];
-#endif
-
-#else
-
-        NSRunAlertPanel(
-            @"Error",
-            [NSString stringWithUTF8String:message],
-            @"OK",
-            nil,
-            nil
-        );
-
+    [alert release];
 #endif
 
 #if !__has_feature(objc_arc)
@@ -50,5 +34,33 @@ void show_error_box(const char *message)
 
 #if __has_feature(objc_arc)
     }
+#endif
+}
+
+#else
+
+static void show_error_box_nsrunalertpanel(const char *message)
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    NSRunAlertPanel(
+        @"Error",
+        [NSString stringWithUTF8String:message],
+        @"OK",
+        nil,
+        nil
+    );
+
+    [pool drain];
+}
+
+#endif
+
+void show_error_box(const char *message)
+{
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 1030
+    show_error_box_nsalert(message);
+#else
+    show_error_box_nsrunalertpanel(message);
 #endif
 }
