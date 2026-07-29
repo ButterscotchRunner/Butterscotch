d52e5a280596bc90af800b5ea8491b00c7ac73c7
Un1q32 <joey.t.reinhart@gmail.com>
2026-07-29 15:25:15 -0400

    consistent behavior of malloc with size 0

--- src/data_win.c
+++ src/data_win.c
@@ -1186,8 +1186,7 @@ static void parseSHDR(BinaryReader* reader, DataWin* dw) {
     Shdr* s = &dw->shdr;
 
     uint32_t* ptrs = readPointerTable(reader, &s->count);
-    if (s->count > 0)
-        s->shaders = (Shader *)safeMalloc(s->count * sizeof(Shader));
+    s->shaders = (Shader *)safeMalloc(s->count * sizeof(Shader));
 
     repeat(s->count, i) {
         // Some GameMaker games have a nullptr for the shader, so we'll just mark them as not-present...
