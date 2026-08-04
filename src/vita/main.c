3d4c94967a8ae06909d3ba87c294a3729660fac2
cobaltgit <cobaltsecuremail312@proton.me>
2026-08-04 11:02:58 +0100

    chore(vita): add platformLog

--- src/vita/main.c
+++ src/vita/main.c
@@ -100,6 +100,25 @@ void handleVitaGamepad(RunnerGamepadState* gp, int port) {
     gp->connectedCount++;
 }
 
+void platformLog(const logType type, const char *format, va_list va) {
+    FILE *out = stderr;
+    switch (type) {
+        case LOG_TYPE_NORMAL:
+            out = stdout;
+            break;
+        case LOG_TYPE_WARNING:
+            fputs("Warning: ", out);
+            break;
+        case LOG_TYPE_ERROR:
+            fputs("Error: ", out);
+            break;
+		case LOG_TYPE_DEBUG:
+            fputs("Debug: ", out);
+            break;
+    }
+    vfprintf(out, format, va);
+}
+
 bool vitaGetWindowSize(int32_t* outW, int32_t* outH) {
     if (!outW || !outH) return false;
     if (currentWindowWidth <= 0 || currentWindowHeight <= 0) return false;
