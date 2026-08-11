52dbc4950052695ab961ad94be93c5472c34bb1f
emiyl <me@emiyl.com>
2026-08-11 16:53:17 +0100

    create qapplication for qt6

--- src/desktop/platform/qt.cpp
+++ src/desktop/platform/qt.cpp
@@ -2,6 +2,12 @@
 #include <QMessageBox>
 
 extern "C" void show_error_box(const char *message) {
+    int argc = 1;
+    char appName[] = "butterscotch";
+    char *argv[] = { appName, nullptr };
+
+    QApplication app(argc, argv);
+
     QMessageBox::critical(
         nullptr,
         "Error",
