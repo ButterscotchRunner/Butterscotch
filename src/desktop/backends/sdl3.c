9f3313a00501cc225bc2b0d0500aa2d1d33b9d65
emiyl <me@emiyl.com>
2026-08-11 17:35:32 +0100

    don't use SDL_ShowSimpleMessageBox cuz it sucks

--- src/desktop/backends/sdl3.c
+++ src/desktop/backends/sdl3.c
@@ -108,13 +108,20 @@ static SDL_Window *tryOpenWindow(int reqW, int reqH, const char* title, Uint32 f
     return NULL;
 }
 
+#if defined(_WIN32) || defined(TARGET_OS_MAC) || defined(LINUX_GUI)
+#define PLATFORM_HAS_ERROR_BOX
+#endif
+
+#ifdef PLATFORM_HAS_ERROR_BOX
+void show_error_box(const char *message);
+#endif
+
 void platformShowErrorDialogue(const char* message) {
-    SDL_ShowSimpleMessageBox(
-        SDL_MESSAGEBOX_ERROR,
-        "Error",
-        message,
-        NULL
-    );
+#ifdef PLATFORM_HAS_ERROR_BOX
+    show_error_box(message);
+#else
+    (void)message; // Suppress unused parameter warning
+#endif
 }
 
 void platformSetWindowTitle(const char* title) {
