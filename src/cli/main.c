aa27b24c97b3f079d4788a7d87e61f585488eca4
Cobalt <65132371+cobaltgit@users.noreply.github.com>
2026-09-04 19:40:03 +0100

    feat(cli): search for WAD in OS-specific dirs like the official runner (#464)
    * feat(cli): search for WAD in OS-specific dirs like the original runner does
    
    for #463
    
    * make it relative to the runner binary dir
    
    * make it look for game.droid too
    
    * readme

--- src/cli/main.c
+++ src/cli/main.c
@@ -527,11 +527,34 @@ static void parseCommandLineArgs(CommandLineArgs* args, int argc, char* argv[]) 
     }
 
     if (optind >= argc) {
-        printUsage(argv[0]);
-        exit(1);
-    }
+        const char* defaultDataWinPaths[4] = {"data.win", "assets/game.unx", "assets/game.droid", "../Resources/game.ios"}; // default WAD paths for Windows/Linux/Android/macOS
+        static char resolvedPath[2048];
 
-    args->dataWinPath = argv[optind];
+        char baseDir[2048] = {0};
+        strncpy(baseDir, argv[0], sizeof(baseDir) - 1);
+        
+        char* lastSlash = strrchr(baseDir, '/');
+        if (!lastSlash) lastSlash = strrchr(baseDir, '\\');
+        
+        if (lastSlash) {
+            *(lastSlash + 1) = '\0';
+        } else {
+            baseDir[0] = '\0';
+        }
+        for (int i = 0; i < 4; i++) {
+            snprintf(resolvedPath, sizeof(resolvedPath), "%s%s", baseDir, defaultDataWinPaths[i]);
+            if (access(resolvedPath, F_OK) == 0) {
+                args->dataWinPath = resolvedPath;
+                break;
+            }
+        }
+        if (args->dataWinPath == nullptr) {
+            printUsage(argv[0]);
+            exit(1);
+        }
+    } else {
+        args->dataWinPath = argv[optind];
+    }
 
 #ifdef ENABLE_SCREENSHOTS
     if (hmlen(args->screenshotFrames) > 0 && args->screenshotPattern == nullptr) {
