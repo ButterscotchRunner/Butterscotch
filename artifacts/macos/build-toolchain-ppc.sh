d0553386d88e6876075bedbdbc32e68ba7870db5
Un1q32 <joey.t.reinhart@gmail.com>
2026-09-14 13:08:03 -0400

    separate out sdk download script

--- artifacts/macos/build-toolchain-ppc.sh
+++ artifacts/macos/build-toolchain-ppc.sh
@@ -9,30 +9,9 @@ cd "$scriptroot"
 platformdir=$PWD
 
 workdir="$PWD/build"
-sdk="$workdir/ppc-sdk"
-mkdir -p "$sdk"
+sdk="$workdir/sdk"
+./dlsdk.sh "$sdk" || exit 1
 cd "$workdir"
-
-# Increase this if we ever make a change to the SDK, for example
-# using a newer SDK version, and we need to invalidate the cache.
-sdkver=1
-if ! [ -d "$sdk" ] || [ "$(cat "$sdk/sdkver" 2>/dev/null)" != "$sdkver" ]; then
-    printf '\nDownloading macOS SDK...\n\n'
-    (
-    # for old stuff
-    [ -d "$sdk" ] && rm -rf "$sdk" &
-    rm -f MacOSX10.5.sdk.tar.xz
-    wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.5.sdk.tar.xz
-    wait
-    tar -xJf MacOSX10.5.sdk.tar.xz
-    mv MacOSX10.5.sdk "$sdk"
-    cd "$sdk"
-    patch -fNp1 < "$platformdir/leopard-sdk-fix.patch"
-    )
-    wait
-    rm ./*.tar.xz
-    printf '%s' "$sdkver" > "$sdk/sdkver"
-fi
 
 if command -v nproc >/dev/null; then
     ncpus="$(nproc)"
